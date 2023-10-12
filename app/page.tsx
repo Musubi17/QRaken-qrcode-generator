e48c0a914fd0e24b8b5f7f42d89f4736a492a2f9
Aleksandr <musubi17@yandex.com>
2023-10-13 05:36:27 +0800

    changes in page.txt file

--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
           {/* <Image src="/logo.png" alt="SuperViral.ai logo" width="500" height="500" /> */}
           {/* <p>Qronos-Code</p> */}
           <h1 className="text-3xl font-bold mb-10">Qronos-Code</h1>
-          <p>This isn't just a QR code. it's your digital signature. Elevate it. Make it iconic.</p>
+          <p>This isn&apos;t just a QR code. it&apos;s your digital signature. Elevate it. Make it iconic.</p>
         </div>
         <Chat />
       </div>
